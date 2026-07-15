33ab9b1f47f0d36d0e1120637e57aaad12027cb3
Bishnu-Macbook-Air <helloatbish@gmail.com>
2026-07-16 01:43:49 +0530

    feat(nvim,tmux): enable mouse drag-to-copy in neovim and tmux

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy to system clipboard on mouse release in Visual mode
+vim.keymap.set("v", "<LeftRelease>", '"+ygv', { desc = "Copy mouse selection to system clipboard" })
